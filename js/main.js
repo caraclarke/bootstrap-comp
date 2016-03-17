99596419da5a049525e078fcdd3765ccd7c1e9af
caraclarke <cara.clarke8@gmail.com>
2016-03-17 09:29:19 -0400

    Fixed router issue warning

--- js/main.js
+++ js/main.js
@@ -24179,21 +24179,16 @@ var Router = require('react-router').Router;
 var Route = require('react-router').Route;
 var Link = require('react-router').Link;
 var IndexRoute = ReactRouter.IndexRoute;
-// var HashHistory = require('react-router/lib/HashHistory').default;
 // var browserHistory = require('react-router').browserHistory;
-
-// router itself that we need
-// individual routes
-// 'show this when someone gets to the homepage'
+var hashHistory = require('react-router').hashHistory;
 
 var BasePage = require('./components/BasePage.jsx');
 var HomePage = require('./components/HomePage.jsx');
 var Jumbotron = require('./components/Jumbotron.jsx');
 
-// <Router history={browserHistory}>
 var Routes = React.createElement(
   Router,
-  null,
+  { history: hashHistory },
   React.createElement(
     Route,
     { path: '/', component: BasePage },
--- src/routes.jsx
+++ src/routes.jsx
@@ -4,20 +4,15 @@ var Router = require('react-router').Router;
 var Route = require('react-router').Route;
 var Link = require('react-router').Link;
 var IndexRoute = ReactRouter.IndexRoute;
-// var HashHistory = require('react-router/lib/HashHistory').default;
 // var browserHistory = require('react-router').browserHistory;
-
-// router itself that we need
-// individual routes
-// 'show this when someone gets to the homepage'
+var hashHistory = require('react-router').hashHistory;
 
 var BasePage = require('./components/BasePage.jsx');
 var HomePage = require('./components/HomePage.jsx');
 var Jumbotron = require('./components/Jumbotron.jsx');
 
-// <Router history={browserHistory}>
 var Routes = (
-  <Router>
+  <Router history={hashHistory}>
     <Route path="/" component={BasePage}>
       <IndexRoute component={HomePage}></IndexRoute>
     </Route>
